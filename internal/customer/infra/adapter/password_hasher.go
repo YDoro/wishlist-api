fc08f17d7644f264f4316752cb383fd3ebb40a1e
yan <yan_mvd@hotmail.com>
2025-05-23 23:44:43 -0300

    feat: :sparkles: create a passwordHasher adapter using bcrypt algorithm

---
+++ internal/customer/infra/adapter/password_hasher.go
@@ -0,0 +1,22 @@
+package adapter
+
+import "golang.org/x/crypto/bcrypt"
+
+type PasswordHasher struct {
+	salt int
+}
+
+func NewPasswordHasher(salt int) *PasswordHasher {
+	return &PasswordHasher{
+		salt: salt,
+	}
+}
+
+// Hash generates a hashed version of the given password using the bcrypt algorithm.
+// The salt value, which determines the computational cost of the hashing process, is
+// provided by the PasswordHasher instance. It returns the hashed password as a string
+// and any potential error encountered during the hashing process.
+func (h *PasswordHasher) Hash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), h.salt)
+	return string(hash), err
+}
