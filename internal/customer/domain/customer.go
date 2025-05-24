4b9ea93b3f369c640620b026f1c96553d776ec72
yan <yan_mvd@hotmail.com>
2025-05-24 00:31:12 -0300

    feat: :sparkles: bringing the model to a closer db aproach

--- internal/customer/domain/customer.go
+++ internal/customer/domain/customer.go
@@ -1,9 +1,21 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type Customer struct {
-	ID       string `json:"id"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+type IncommingCustomer struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
@@ -14,5 +26,5 @@ type CustomerRepository interface {
 }
 
 type CustomerUC interface {
-	CreateCustomerWithEmail(ctx context.Context, email string, name string) (string, error)
+	CreateCustomerWithEmail(ctx context.Context, data IncommingCustomer) (string, error)
 }
