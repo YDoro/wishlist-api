867d7a913850840ce55ee51adf7928cd4f74cbd5
yan <yan_mvd@hotmail.com>
2025-05-24 00:30:08 -0300

    feat: :sparkles: add password field to the request body

--- pkg/wishlist/presentation/inputs/customer.go
+++ pkg/wishlist/presentation/inputs/customer.go
@@ -1,6 +1,7 @@
 package inputs
 
 type CreateCustomerRequest struct {
-	Name  string `json:"name" binding:"required"`
-	Email string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password"`
 }
