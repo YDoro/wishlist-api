160c6810afa5003e83b326e3924f5707b3d5cfc4
yan <yan_mvd@hotmail.com>
2025-05-26 13:42:55 -0300

    feat: :sparkles: create an updateCustomer implementations

---
+++ internal/usecase/update_customer_usecase.go
@@ -0,0 +1,69 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/ydoro/wishlist/internal/domain"
+	e "github.com/ydoro/wishlist/internal/presentation/errors"
+)
+
+type UpdateCustomerUseCase struct {
+	Updater     domain.UpdateCustomerRepository
+	Getter      domain.GetCustomerByIDRepository
+	EmailGetter domain.GetCustomerByEmailRepository
+}
+
+func NewUpdateCustomerUseCase(
+	updater domain.UpdateCustomerRepository,
+	getter domain.GetCustomerByIDRepository,
+	emailGetter domain.GetCustomerByEmailRepository,
+) *UpdateCustomerUseCase {
+	return &UpdateCustomerUseCase{
+		Updater:     updater,
+		Getter:      getter,
+		EmailGetter: emailGetter,
+	}
+}
+
+func (u *UpdateCustomerUseCase) UpdateCustomer(ctx context.Context, currentCustomerID string, customerID string, data domain.CustomerEditableFields) (*domain.OutgoingCustomer, error) {
+	if currentCustomerID != customerID {
+		return nil, e.NewUnauthorizedError()
+	}
+	customer, err := u.Getter.GetByID(ctx, customerID)
+	if err != nil {
+		return nil, err
+	}
+
+	if data.Name != "" {
+		customer.Name = data.Name
+	}
+
+	if data.Email != "" {
+		if customer.Email != data.Email {
+			existingCustomer, err := u.EmailGetter.GetByEmail(ctx, data.Email)
+			if err != nil {
+				return nil, err
+			}
+
+			if existingCustomer != nil && existingCustomer.ID != customerID {
+				return nil, &e.ValidationError{Field: "email", Err: "email already in use"}
+			}
+
+			customer.Email = data.Email
+		}
+	}
+
+	err = u.Updater.Update(ctx, customer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.OutgoingCustomer{
+		ID:        customer.ID,
+		Name:      customer.Name,
+		Email:     customer.Email,
+		CreatedAt: customer.CreatedAt,
+	}, nil
+
+}
